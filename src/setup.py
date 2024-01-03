3f1df47e45f9dae43b9a248c4b0e902d963d0730
Ibrahim Animashaun <iaanimashaun@gmail.com>
2024-01-03 22:17:35 +0000

    update

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="hailstone_calculator",
-    version="0.0.16",
+    version="0.0.17",
     packages=find_packages(exclude=["tests*"]),
     license="MIT",
     description="Hailstone calculator",
